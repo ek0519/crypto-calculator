374fb4c58a0b3a66f424ac080c3f4da3bbc96c0a
ekman <ek0519@gmail.com>
2025-07-04 10:39:28 +0800

    feat: 🎸 add other api

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -5,11 +5,104 @@ import type { App } from "../../../crypto-backend/src/app";
 
 const client = treaty<App>(env.PUBLIC_API_URL);
 
-export const dashboard = async (access_token: string) => {
-  const response = await client.dashboard.get({
+const options = (access_token: string) => {
+  return {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
-  });
+  };
+};
+
+interface BasePrams {
+  access_token: string;
+}
+
+export const dashboard = async (access_token: string) => {
+  const response = await client.dashboard.get(options(access_token));
   return response.data;
+};
+
+export const login = async (email: string) => {
+  const response = await client.auth.login.post({
+    email,
+  });
+  if (response.status !== 200) {
+    throw new Error("Login failed");
+  }
+  return response.data;
+};
+
+export enum Direction {
+  BUY = "BUY",
+  SELL = "SELL",
+}
+
+export interface CreateTransactionParams extends BasePrams {
+  symbol: string;
+  amount: number;
+  price: number;
+  note?: string;
+  direction: "BUY" | "SELL";
+}
+
+export const createTransaction = async ({
+  access_token,
+  symbol,
+  amount,
+  price,
+  note,
+  direction,
+}: CreateTransactionParams) => {
+  const response = await client.transcations.post({
+    symbol,
+    amount,
+    price,
+    note,
+    direction,
+    ...options(access_token),
+  });
+  if (response.status !== 200) {
+    throw new Error("Transaction creation failed");
+  }
+  return response.data;
+};
+
+export const getTransaction = async (access_token: string, id: string) => {
+  const response = await client["transcations:id"].get({
+    query: {
+      id,
+    },
+  });
+  if (response.status !== 200) {
+    throw new Error("Transaction get failed");
+  }
+  return response.data;
+};
+
+export interface GetTransactionsParams extends BasePrams {
+  page?: number;
+  limit?: number;
+  symbol: string;
+  from?: Date;
+  to?: Date;
+}
+
+export const getTransactions = async ({
+  access_token,
+  page = 1,
+  limit = 10,
+  symbol,
+  from,
+  to,
+}: GetTransactionsParams) => {
+  const response = await client.transcations.get({
+    query: {
+      page,
+      limit,
+      symbol,
+      from,
+      to,
+    },
+    ...options(access_token),
+  });
 };
--- src/routes/(public)/login/+page.server.ts
+++ src/routes/(public)/login/+page.server.ts
@@ -1,7 +1,7 @@
-import { env } from "$env/dynamic/private";
 import { type Actions, fail, redirect } from "@sveltejs/kit";
 import { getUserProfile } from "../../../hooks.server";
 import type { PageServerLoad } from "./$types";
+import { login } from "../../../api/backend";
 export const load: PageServerLoad = async ({ url, locals, cookies }) => {
   const accessToken =
     url.searchParams.get("access_token") || cookies.get("access_token");
@@ -24,19 +24,7 @@ export const actions: Actions = {
     const data = await request.formData();
     const email = data.get("email")?.toString();
     if (!email) return fail(400, { email, incorrect: true });
-    const apiUrl = env.PUBLIC_API_URL;
-
-    // 向外部 API 發送登入請求
-    const response = await fetch(`${apiUrl}/auth/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email }),
-      credentials: "include", // 確保接收 API 返回的 cookie
-    });
-
-    if (!response.ok) {
-      return fail(401, { error: "Email was invalid" });
-    }
+    await login(email);
     return redirect(303, "/login?success=true");
   },
 };
